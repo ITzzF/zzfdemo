6ec6dfb6bd9471577d67420152309a1f1526373e
张智帆 <10035567+Zhangzhi_fan@user.noreply.gitee.com>
2024-10-08 19:07:30 +0800

    使用ipyleaflet添加Map类

--- zzfdemo/zzfdemo.py
+++ zzfdemo/zzfdemo.py
@@ -1,1 +1,8 @@
 """Main module."""
+
+import ipyleaflet
+class Map(ipyleaflet.Map):
+    
+    def __init__(self,center=[34.32, 108.55],zoom=4, **kwargs):
+        super().__init__(center = center,zoom = zoom ,**kwargs)
+        self.add_control(ipyleaflet.LayersControl())

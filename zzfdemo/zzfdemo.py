7285ef7a38762531ab0bcded657162136520a61c
张智帆 <10035567+Zhangzhi_fan@user.noreply.gitee.com>
2024-10-09 15:36:57 +0800

    更新文件路径,增加ipyleaflet案例

--- zzfdemo/zzfdemo.py
+++ zzfdemo/zzfdemo.py
@@ -1,8 +1,22 @@
 """Main module."""
 
 import ipyleaflet
+from ipyleaflet import basemaps
 class Map(ipyleaflet.Map):
     
     def __init__(self,center=[34.32, 108.55],zoom=4, **kwargs):
         super().__init__(center = center,zoom = zoom ,**kwargs)
         self.add_control(ipyleaflet.LayersControl())
+    
+    def add_tile_layer(self,url,name , **kwargs):
+        layer = ipyleaflet.TileLayer(url = url,name = name ,**kwargs)
+        self.add_layer(layer)
+
+    def add_basemap(self,name):
+        if isinstance(name,str):
+            url = eval(f'basemaps.{name}').build_url()
+            self.add_tile_layer(url,name)
+        else:
+            self.add_layer(name)
+
+    

c0110787d45e4857859a7a8a24f3ed47f4419eac
张智帆 <10035567+Zhangzhi_fan@user.noreply.gitee.com>
2024-10-10 11:02:46 +0800

    添加矢量文件以及操作矢量文件的方法

--- zzfdemo/zzfdemo.py
+++ zzfdemo/zzfdemo.py
@@ -1,7 +1,8 @@
 """Main module."""
 
 import ipyleaflet
-from ipyleaflet import basemaps
+from ipyleaflet import basemaps,GeoJSON
+import json
 class Map(ipyleaflet.Map):
     
     def __init__(self,center=[34.32, 108.55],zoom=4, **kwargs):
@@ -19,4 +20,39 @@ class Map(ipyleaflet.Map):
         else:
             self.add_layer(name)
 
+    def add_geojson(self,fileName,name = 'geojson',**kwargs):
+        if isinstance(fileName,str):
+            with open(f'{fileName}') as f:
+                data = json.load(f)
+
+        if "style" not in kwargs:
+            kwargs["style"] = {
+                'opacity': 1, 'dashArray': '9', 'fillOpacity': 0.1, 'weight': 1
+            }
+        if "hover_style" not in kwargs:
+            kwargs["hover_style"] = {
+                'color': 'red', 'dashArray': '0', 'fillOpacity': 0.5
+            }
+        layer = GeoJSON(data = data,name = name ,**kwargs)
+        self.add(layer)
+
+
+    def add_shapefile(self,fileName,name = 'shapefile',**kwargs):
+        import shapefile
+    
+
+        if isinstance(fileName,str):
+            with shapefile.Reader(fileName) as shp:
+                data = shp.__geo_interface__
+
+        if "style" not in kwargs:
+            kwargs["style"] = {
+                'opacity': 1, 'dashArray': '9', 'fillOpacity': 0.1, 'weight': 1
+            }
+        if "hover_style" not in kwargs:
+            kwargs["hover_style"] = {
+                'color': 'red', 'dashArray': '0', 'fillOpacity': 0.5
+            }
+        layer = GeoJSON(data = data,name = name ,**kwargs)
+        self.add(layer)
     
